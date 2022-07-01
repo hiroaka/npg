988e84a0e35ee41b65b9dc08c7ba1d6404eacadc
Renan Praxedes <renan.praxedes@gmail.com>
2022-07-01 17:02:59 -0300

    yarn loick

--- src/components/layouts/Landing.js
+++ src/components/layouts/Landing.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import PassForm from "../PassForm";
 
 class Landing extends Component {
     render() {
         return (
             <div>
                 <h1>Welcome</h1>
+                <div className="container">
+                    <PassForm/>
+                </div>
             </div>
         );
     }
